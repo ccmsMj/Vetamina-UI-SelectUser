3728729dd6b5ec9d556bf31392e32dcaf11d85b8
Marvic Joshua Villaverde <joshuavillaverde29@gmail.com>
2022-11-02 23:42:07 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import vet from '../src/components/veterinarian.png';
 import emp from '../src/components/employee.png';
 import logo from '../src/components/logo.png';
 import paw from '../src/components/paw.png';
+import owner from '../src/components/owner.png';
+import admin from '../src/components/admin.png';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 
@@ -10,10 +12,14 @@ function App() {
     <div>
         <img src={logo} className="logo" alt="logo" />
         <img src={paw} className="paw" alt="paw" />
-        <img src={vet} className="picture" alt="vet" />
-        <img src={emp} className="picture" alt="emp" /><br/>
-        <Button variant="success" className="button1">VETERINARIAN</Button>
-        <Button variant="success" className="button2">EMPLOYEE</Button><br/>
+        <img src={admin} className="picture1" alt="admin" />
+        <img src={owner} className="picture2" alt="owner" />
+        <img src={emp} className="picture3" alt="emp" />
+        <img src={vet} className="picture4" alt="vet" /><br/>
+        <Button variant="success" className="button1">ADMIN</Button>
+        <Button variant="success" className="button2">CLINIC OWNER</Button>
+        <Button variant="success" className="button3">EMPLOYEE</Button>
+        <Button variant="success" className="button4">VETERINARIAN</Button>
       
         <a className='select'>Select type of User</a>
   
